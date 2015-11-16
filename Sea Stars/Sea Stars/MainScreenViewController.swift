9471cd1a02a5725a01ee0365ab7e181ca41d6e63
Carson Carroll <ccarroll511@yahoo.com>
2015-11-16 15:30:23 -0800

    Got images to display for the 5 quick picks

--- Sea Stars/Sea Stars/MainScreenViewController.swift
+++ Sea Stars/Sea Stars/MainScreenViewController.swift
@@ -7,18 +7,75 @@
 //
 
 import UIKit
+import Parse
 
 class MainScreenViewController: UIViewController, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var imageButton1: UIButton!
+    @IBOutlet weak var imageButton2: UIButton!
+    @IBOutlet weak var imageButton3: UIButton!
+    @IBOutlet weak var imageButton4: UIButton!
+    @IBOutlet weak var imageButton5: UIButton!
+    
+    var seaStarImages:[UIImage] = [UIImage]()
+    var species:[String] = [String]()
+
     var observer_name: String?
     var site_location: String?
     var report_date: String?
+    
+    var imageButtonCounter = 0
+    
+    func convertFirstElementToImage(object:AnyObject, imageNumber:Int) -> Void {
+        let imagesArray:[PFFile] = (object as! PFObject)["images"] as! [PFFile]
+        let imageFile:PFFile = imagesArray[0]
+
+        do {
+            let imageData = try imageFile.getData()
+            let seaStarImage:UIImage = UIImage(data: imageData)!
+            self.seaStarImages.append(seaStarImage)
+            
+            switch (imageNumber) {
+                case 0:
+                    self.imageButton1.setBackgroundImage(self.seaStarImages[imageNumber], forState: .Normal)
+                case 1:
+                    self.imageButton2.setBackgroundImage(self.seaStarImages[imageNumber], forState: .Normal)
+                case 2:
+                    self.imageButton3.setBackgroundImage(self.seaStarImages[imageNumber], forState: .Normal)
+                case 3:
+                    self.imageButton4.setBackgroundImage(self.seaStarImages[imageNumber], forState: .Normal)
+                case 4:
+                    self.imageButton5.setBackgroundImage(self.seaStarImages[imageNumber], forState: .Normal)
+                default:
+                    break
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "addCreature")
+        
+        let speciesQuery = PFQuery(className: "Species")
+        speciesQuery.limit = 5
+        speciesQuery.findObjectsInBackgroundWithBlock{ (objects, error) -> Void in
+            if error == nil {
+                for object in objects! {
+                    self.convertFirstElementToImage(object, imageNumber:self.imageButtonCounter++)
+                    let speciesName:String = (object as PFObject)["name"] as! String
+                    self.species.append(speciesName)
+                }
+            }
+            else {
+                print("Error: \(error) \(error!.userInfo)")
+            }
+        }
     }
     
     func addCreature() -> Void {
