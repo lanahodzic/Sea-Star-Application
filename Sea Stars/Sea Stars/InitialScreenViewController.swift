0f077633b4bf328f0873c82111852c9935d6f32b
lanahodzic <lana@enoteway.com>
2015-11-23 15:57:00 -0800

    beautiful app :)

--- Sea Stars/Sea Stars/InitialScreenViewController.swift
+++ Sea Stars/Sea Stars/InitialScreenViewController.swift
@@ -9,24 +9,7 @@
 import UIKit
 import Parse
 
-class Report {
-    var observer: String?
-    var site: String?
-    var species: [String]?
-    var date: String?
-}
-
 class ReportTableViewCell: UITableViewCell {
-    var report: Report? {
-        didSet {
-            if let new_report = report {
-                reportDate.text = new_report.date!
-                reportLocation.text = new_report.site!
-                reportObserver.text = new_report.observer!
-            }
-        }
-    }
-    
     @IBOutlet weak var reportDate: UILabel!
     @IBOutlet weak var reportLocation: UILabel!
     @IBOutlet weak var reportObserver: UILabel!
@@ -35,12 +18,58 @@ class ReportTableViewCell: UITableViewCell {
 class InitialScreenViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var latestReports: UITableView!
-    var reports: [Report] = [Report]()
+    var reportDictionary:[[String:AnyObject]] = [[String:AnyObject]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         // Do any additional setup after loading the view.
+        
+        latestReports.dataSource = self
+        latestReports.delegate = self
+        
+        reportDictionary.removeAll()
+        
+        let reportQuery = PFQuery(className: "Reports")
+        reportQuery.orderByDescending("date")
+        reportQuery.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
+            if error == nil {
+                for object in objects! as [PFObject] {
+                    var dictionary = [String:AnyObject]()
+                    dictionary["observer"] = object["observer"] as? String
+                    let dateFormatter = NSDateFormatter()
+                    dateFormatter.dateFormat = "MM/dd/yyyy"
+                    dictionary["date"] = dateFormatter.stringFromDate((object["date"] as? NSDate)!)
+                    dictionary["site"] = object["site"] as? String
+                    
+                    let reportXSpeciesQuery = PFQuery(className: "ReportXSpecies")
+                    reportXSpeciesQuery.whereKey("reportID", equalTo: object.objectId!)
+                    reportXSpeciesQuery.orderByAscending("piling")
+                    reportXSpeciesQuery.addAscendingOrder("depth")
+                    reportXSpeciesQuery.addAscendingOrder("rotation")
+                    
+                    do {
+                        dictionary["reportItems"] = try reportXSpeciesQuery.findObjects()
+                        
+                    }
+                    catch {
+                        print("Error: \(error)")
+                    }
+                    
+                    self.reportDictionary.append(dictionary)
+                }
+                
+                self.latestReports.reloadData()
+            }
+            else {
+                print("Error: \(error) \(error!.userInfo)")
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -53,14 +82,19 @@ class InitialScreenViewController: UIViewController, UITableViewDataSource, UITa
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return reports.count
+        return reportDictionary.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = self.latestReports.dequeueReusableCellWithIdentifier("reportCell", forIndexPath: indexPath) as! ReportTableViewCell
-        let report = reports[indexPath.row]
+        print("\(cell) \n")
+        print("\(indexPath.row) \n")
+        let report = reportDictionary[indexPath.row]
+        print("\(report) \n")
         
-        cell.report = report
+        cell.reportLocation.text = (report["site"] as! String)
+        cell.reportDate.text = (report["date"] as! String)
+        cell.reportObserver.text = (report["observer"] as! String)
         /*
         //set the cell info to the report info
         if let site = report.site {
@@ -78,14 +112,18 @@ class InitialScreenViewController: UIViewController, UITableViewDataSource, UITa
         return cell
     }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if  segue.identifier == "reportview" {
+            let reportVC = segue.destinationViewController as! ReportViewController
+            reportVC.report = reportDictionary[latestReports.indexPathForSelectedRow!.row]
+        }
     }
-    */
+    
 
 }
