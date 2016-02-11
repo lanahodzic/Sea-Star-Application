6bc518a6215d06dfbce72f7b1accbe36f5d22997
Chris Wu <cjwu15@gmail.com>
2016-02-10 16:12:05 -0800

    change site and name to dropdown fields

--- Sea Stars/Sea Stars/StartTrackingViewController.swift
+++ Sea Stars/Sea Stars/StartTrackingViewController.swift
@@ -9,29 +9,38 @@
 import UIKit
 import Parse
 
-class StartTrackingViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class StartTrackingViewController: UIViewController {
 
-    @IBOutlet weak var site: UIPickerView!
-    @IBOutlet weak var observer_name: UIPickerView!
+    @IBOutlet weak var site: KSTokenView!
+    @IBOutlet weak var observer_name: KSTokenView!
     @IBOutlet weak var report_date: UIDatePicker!
-    
-    
+
+
     var siteData: [String] = [String]()
     var names: [String] = [String]()
-    
+
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
 
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.site.delegate = self
-        self.site.dataSource = self
+        self.site.promptText = "Site: "
+        self.site.maxTokenLimit = 1
+        self.site.style = .Squared
+        self.site.searchResultSize = CGSize(width: self.site.frame.width, height: 120)
+        self.site.font = UIFont.systemFontOfSize(17)
+
         self.observer_name.delegate = self
-        self.observer_name.dataSource = self
-        
+        self.observer_name.promptText = "Name: "
+        self.observer_name.maxTokenLimit = 1
+        self.observer_name.style = .Squared
+        self.observer_name.searchResultSize = CGSize(width: self.site.frame.width, height: 120)
+        self.observer_name.font = UIFont.systemFontOfSize(17)
+
         let site_query = PFQuery(className: "Sites")
         site_query.findObjectsInBackgroundWithBlock{ (objects, error) -> Void in
             if error == nil {
@@ -45,13 +54,12 @@ class StartTrackingViewController: UIViewController, UIPickerViewDataSource, UIP
                 
                 print("Size of siteData: \(self.siteData.count)")
                 print("Successfully retrieved: \(objects)")
-                self.site.reloadAllComponents()
             } else {
                 print("Error: \(error) \(error!.userInfo)")
             }
         }
-        
-       let name_query = PFQuery(className: "Observer")
+
+        let name_query = PFQuery(className: "Observer")
         name_query.findObjectsInBackgroundWithBlock { (objects, error) -> Void in
             if error == nil {
                 for object in objects! {
@@ -60,11 +68,10 @@ class StartTrackingViewController: UIViewController, UIPickerViewDataSource, UIP
                     let name = first_name! + " " + last_name!
                     self.names.append(name)
                 }
-                self.observer_name.reloadAllComponents()
             } else {
                 print("Error: \(error) \(error!.userInfo)")
             }
-            
+
         }
     }
 
@@ -72,43 +79,32 @@ class StartTrackingViewController: UIViewController, UIPickerViewDataSource, UIP
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        if (pickerView.tag == 0){
-            return siteData.count
+
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        if self.observer_name.tokens() == nil || self.observer_name.tokens()!.isEmpty {
+            return false
         }
-        else {
-            return names.count
+
+        if self.site.tokens() == nil || self.site.tokens()!.isEmpty {
+            return false
         }
-    }
-    
-    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        if (pickerView.tag == 0){
-            return siteData[row]
-        }
-        else {
-            return names[row]
-        }
-        
+
+        return true
     }
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-       let vc = segue.destinationViewController as! MainScreenViewController
-        let observer_row = self.observer_name.selectedRowInComponent(0)
-        vc.observer_name = self.names[observer_row]
-        
-        let site_row = self.site.selectedRowInComponent(0)
-        vc.site_location = self.siteData[site_row]
-        
+        let vc = segue.destinationViewController as! MainScreenViewController
+        vc.observer_name = self.observer_name.tokens()![0].title
+        print("*** NAME: " + vc.observer_name!)
+
+        vc.site_location = self.site.tokens()![0].title
+        print("*** SITE: " + vc.site_location!)
+
         let dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "dd-MM-yyyy"
         let date = dateFormatter.stringFromDate(self.report_date.date)
         vc.report_date = date
-        
+
     }
 
     /*
@@ -116,9 +112,27 @@ class StartTrackingViewController: UIViewController, UIPickerViewDataSource, UIP
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
 
 }
+
+extension StartTrackingViewController: KSTokenViewDelegate {
+    func tokenView(token: KSTokenView, performSearchWithString string: String, completion: ((results: Array<AnyObject>) -> Void)?) {
+        var matches: Array<String> = []
+        let data = token.tag == 0 ? self.siteData : self.names
+        for value: String in data {
+            if value.lowercaseString.rangeOfString(string.lowercaseString) != nil {
+                matches.append(value)
+            }
+        }
+        completion!(results: matches)
+    }
+
+    func tokenView(token: KSTokenView, displayTitleForObject object: AnyObject) -> String {
+        return object as! String
+    }
+}
+
